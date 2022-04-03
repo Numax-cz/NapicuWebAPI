e1993bc929070e610206e2d52f6f89b75cf015d4
Marcel <numax.cz@protonmail.ch>
2022-04-03 11:55:06 +0200

    První commit

---
+++ src/main/java/com/napicu/napicuwebapi/NapicuWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.napicu.napicuwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapicuWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NapicuWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/napicu/napicuwebapi/NapicuWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napicu.napicuwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapicuWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
