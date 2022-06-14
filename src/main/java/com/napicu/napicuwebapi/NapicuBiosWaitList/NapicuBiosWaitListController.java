8b837ea3c60d028b7b9107118b6be611dbb62597
Marcel <numax.cz@protonmail.ch>
2022-06-14 18:47:14 +0200

    Přidána dokumentace

--- src/main/java/com/napicu/napicuwebapi/NapicuBiosWaitList/NapicuBiosWaitListController.java
+++ src/main/java/com/napicu/napicuwebapi/NapicuBiosWaitList/NapicuBiosWaitListController.java
@@ -4,8 +4,13 @@ import com.napicu.napicuwebapi.NapicuIP.NapicuIPService;
 import com.napicu.napicuwebapi.NapicuPopJonanek.NapicuPopJonanekPostModel;
 import com.napicu.napicuwebapi.NapicuPopJonanek.NapicuPopJonanekResponseModel;
 import com.napicu.napicuwebapi.exception.RequestException;
+import com.napicu.napicuwebapi.exception.RequestExceptionSchema;
 import com.napicu.napicuwebapi.service.RateLimit;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +26,14 @@ public class NapicuBiosWaitListController {
     private RateLimit rateLimit;
 
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vše je v pořádku"),
+            @ApiResponse(responseCode = "429", description = "Příliš mnoho požadavků",
+                    content = @Content(mediaType = "application/json",schema = @Schema(implementation = RequestExceptionSchema.class))),
+            @ApiResponse(responseCode = "500", description = "Chyba databáze",
+                    content = @Content(mediaType = "application/json",schema = @Schema(implementation = RequestExceptionSchema.class))),
+    }
+    )
     @PostMapping("/waitlist")
     public NapicuBiosWaitListResponseModel post(@RequestBody NapicuBiosWaitListModel data) {
         if (rateLimit.getServiceBucket().tryConsume(1)) {
