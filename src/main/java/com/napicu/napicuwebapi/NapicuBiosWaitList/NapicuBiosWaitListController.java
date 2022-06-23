e58323516f298b1f83bf2e20fde61aeae4804728
Marcel <numax.cz@protonmail.ch>
2022-06-23 15:39:20 +0200

    Opraveny chyba při body

--- src/main/java/com/napicu/napicuwebapi/NapicuBiosWaitList/NapicuBiosWaitListController.java
+++ src/main/java/com/napicu/napicuwebapi/NapicuBiosWaitList/NapicuBiosWaitListController.java
@@ -8,12 +8,12 @@ import com.napicu.napicuwebapi.exception.RequestExceptionSchema;
 import com.napicu.napicuwebapi.service.RateLimit;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.napicu.napicuwebapi.exception.NapicuExceptions.NAPICU_TO_MANY_REQUESTS;
